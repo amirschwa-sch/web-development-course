3e7993fe9083ffe857dac82e97b4710c19b642e4
Amir Schwartz <amirschwa@gmail.com>
2026-03-13 04:27:25 +0200

    Add Flask students CRUD project

---
+++ students_app/app/static/main.js
@@ -0,0 +1,64 @@
+"use strict";
+
+{
+    const tbody = document.getElementById("tbody");
+
+    const idBox = document.getElementById("idBox");
+    const nameBox = document.getElementById("nameBox");
+    const ageBox = document.getElementById("ageBox");
+    const btAdd = document.getElementById("btAdd");
+    const btUpdate = document.getElementById("btUpdate");
+    const btDelete = document.getElementById("btDelete");
+
+    window.addEventListener("load", getALL);
+
+    btAdd.addEventListener("click", async function () {
+        const name = nameBox.value;
+        const age = ageBox.value;
+        const student = { name, age };
+
+        if (!name || !age) {
+            alert("Enter name and age");
+            return;
+        }
+
+        await axios.post("/students", student);
+        await getALL();
+    });
+
+    btDelete.addEventListener("click", async function () {
+        const id = idBox.value;
+
+        if (!id) {
+            alert("Enter ID!");
+            return;
+        }
+
+        try {
+            await axios.delete(`/students/${id}`);
+            await getALL();
+            idBox.value = "";
+        } catch (error) {
+            console.error(error);
+            alert(error.response?.data?.message || "Delete failed");
+        }
+    });
+
+    async function getALL() {
+        let response = await axios.get("/students");
+        const students = response.data;
+        let content = "";
+
+        for (const s of students) {
+            content += `
+                <tr>
+                    <td>${s.id}</td>
+                    <td>${s.name}</td>
+                    <td>${s.age}</td>
+                </tr>
+            `;
+        }
+
+        tbody.innerHTML = content;
+    }
+}
